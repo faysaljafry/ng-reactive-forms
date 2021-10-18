65231b72c3d78909cd25bb872a6e7b0cfca0dee7
faysaljafry <faysaljafryofficial@gmail.com>
2021-10-18 18:04:00 +0500

    ng-Forms reative lecture Paractice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormArray,
+  FormBuilder,
+  AbstractControl,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'ng-reactive-forms';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.signUpForm = new FormGroup({
+      userData: new FormGroup({
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this),
+        ]),
+        email: new FormControl(
+          null,
+          [Validators.required, Validators.email],
+          this.forbiddenEmails
+        ),
+      }),
+      gender: new FormControl('male'),
+      hobbies: new FormArray([]),
+    });
+  }
+  genders = ['male', 'female'];
+  signUpForm!: FormGroup;
+  forbiddenusernames = ['Chriss', 'Anna'];
+
+  constructor(private formBuilder: FormBuilder) {}
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
+  get hobbies() {
+    return this.signUpForm.get('hobbies') as FormArray;
+  }
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signUpForm.get('hobbies')).push(control);
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenusernames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    }
+    return null as any;
+  }
+
+  forbiddenEmails(control: AbstractControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ emailIsForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
 }
